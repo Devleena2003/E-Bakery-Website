727bebb33b2a949307ae36ba0e78f2cd46442226
Devleena Das <95465835+Devleena2003@users.noreply.github.com>
2022-09-03 17:40:24 +0530

    Add files via upload

---
+++ Sign Up/signUp.js
@@ -0,0 +1,13 @@
+
+let btn = document.querySelector("#button");
+
+   function myfunction(){
+    let name= document.getElementById('name').value;
+    let email= document.getElementById('email').value;
+    let password= document.getElementById('password').value;
+    console.log(name);
+    console.log(email);
+    console.log(password);
+}
+
+
