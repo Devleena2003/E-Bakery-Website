95be8695e00dbf3a338144a2afdf3d0e591090b2
Debayan <pradhandebayan@gmail.com>
2022-09-09 13:15:41 +0530

    linked login regiser landing

--- Sign Up/signUp.js
+++ Sign Up/signUp.js
@@ -1,13 +1,31 @@
 
-let btn = document.querySelector("#button");
+const form = document.getElementById("formelement");
+form.addEventListener('submit',registerUser);
 
-   function myfunction(){
-    let name= document.getElementById('name').value;
-    let email= document.getElementById('email').value;
-    let password= document.getElementById('password').value;
-    console.log(name);
-    console.log(email);
-    console.log(password);
-}
+   async function registerUser(event){
+    event.preventDefault();
+    const username= document.getElementById('name').value;
+    const email= document.getElementById('email').value;
+    const password= document.getElementById('password').value;
+    // console.log(name);
+    // console.log(email);
+    // console.log(password);
 
+    const userData = {username,email,password}
+    console.log(userData)
 
+    const result = await fetch('https://evening-refuge-31987.herokuapp.com/api/auth/register/',{
+        method: 'POST',
+        mode:'cors',
+        headers:{
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    }).then((res)=>res.json());
+
+    // console.log(result);
+
+    if(result.success){
+        window.location.assign('../index.html')
+    }
+   }
