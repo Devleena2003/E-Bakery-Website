920b30806156c1fa200a97c64deef99cb09ea706
Debayan <pradhandebayan@gmail.com>
2022-09-09 20:19:47 +0530

    updated breaking change

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ async function loginUser(event){
     const username = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    console.log(username,password);
+    // console.log(username,password);
     const data = {username,password};
     // console.log(data);
 
-    const result = await fetch('http://localhost:3000/api/auth/login',{
+    const result = await fetch('https://evening-refuge-31987.herokuapp.com/api/auth/login/',{
         method: 'POST',
         mode:'cors',
         headers:{
@@ -22,7 +22,7 @@ async function loginUser(event){
     // console.log(result.success);
     
     if(result.success){
-        window.location.assign("next.html");  //next.html will be the redirect page if login is succesfull
+        window.location.assign("./Landing/landing.html");  //next.html will be the redirect page if login is succesfull
     }
 }
 
