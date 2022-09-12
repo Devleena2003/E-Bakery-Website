c00160032bf7310e2da3b2385bc59d1afb52764c
Debayan <pradhandebayan@gmail.com>
2022-09-12 17:23:18 +0530

    added delte cart item functionality

--- cartspage/main.js
+++ cartspage/main.js
@@ -30,13 +30,35 @@ fetch(`https://evening-refuge-31987.herokuapp.com/api/carts/${localStorage.getIt
             const prodimg = prodcard.querySelector('.prodimg')
 
             prodname.textContent = proditem.title
+            prodname.classList.add(proditem._id)
             prodprice.textContent = proditem.price
             proddesc.textContent = proditem.description
             prodimg.src = proditem.image
 
             maincontainer.append(prodcard)
 
-        })
+        })        
+    })    
+  })  
+  
+async function removefunc(e){
+    // console.log(e)
+    // console.log(e.path[3].children[0].classList[1])
+
+    const productId = e.path[3].children[0].classList[1]
+    const data = {productId}
+
+    fetch(`http://localhost:5000/api/carts/${localStorage.getItem("userid")}`,{
+        method:'PUT',
+        mode:'cors',
+        headers:{
+            'Content-type': 'application/json',
+            'token' : `Bearer ${localStorage.getItem("accessToken")}`
+        },
+        body:JSON.stringify(data)
+    }).then(res=>res.json())
+      .then(data=>{
+        console.log(data)
+        window.location.assign('./index.html')
     })
-    
-  })
+}
