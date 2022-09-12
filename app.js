32c98120336631cc10f5cf28269ddf04e55013bf
Debayan <pradhandebayan@gmail.com>
2022-09-12 10:29:56 +0530

    update cart value for logged in users across all pages

--- app.js
+++ app.js
@@ -23,9 +23,27 @@ async function loginUser(event){
     if(result.success){
         localStorage.setItem("accessToken",result.accessToken)
         localStorage.setItem("userid",result.others._id)
-        localStorage.setItem("cartvalue",0)
-        window.location.assign("./Landing/landing.html"); 
-    }
+        fetch(`https://evening-refuge-31987.herokuapp.com/api/carts/${result.others._id}`,{
+            method: 'GET',
+            moder:'cors',
+            headers:{
+                'Content-type': 'application/json',
+                'token': `Bearer ${result.accessToken}`
+            }
+        }).then(res => res.json())
+          .then(data => {
+            if(data.cart === null){
+                localStorage.setItem("cartvalue",0)
+            }else{
+                console.log(data)
+                localStorage.setItem("cartvalue",data.cart.products.length)
+            }
+            window.location.assign("./Landing/landing.html"); 
+          })
+
+        // localStorage.setItem("cartvalue",0)
+        // window.location.assign("./Landing/landing.html"); 
+        }
 }
 
  
